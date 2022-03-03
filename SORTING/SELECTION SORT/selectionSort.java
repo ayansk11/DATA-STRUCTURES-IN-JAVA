373558116683eaff5e6d0fde8c98c6da18c011ce
AYANSK11 <92205708+ayansk11@users.noreply.github.com>
2022-03-03 18:29:30 +0530

    Update selectionSort.java

--- SORTING/SELECTION SORT/selectionSort.java
+++ SORTING/SELECTION SORT/selectionSort.java
@@ -19,15 +19,38 @@ public class selectionSort {
 
             System.out.print(arr[i] + "\t");
         }
-       
+        System.out.println("\n");
         
-        System.out.println("THE ELEMENTS IN THE ARRAY ARE: ");
+        System.out.println("PERFORMING INSERTION SORT ON THE ARRAY........");
+        sort(arr);
+        
+        System.out.println("PRINTING THE SORTED ARRAY..........");
         for (int i = 0; i <= arr.length - 1; i++) {
 
             System.out.print(arr[i] + "\t");
         }
     }
+    
+    void sort(int[] arr) {
 
+        int min;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[min]) {
+
+                    min = j;
+                }
+            }
+        
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+    }
    
 }
 
