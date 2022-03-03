e865e1854014694d35fd56015eed83d76f35c269
Mohammad faizan <90141012+Faizanmadgaonwala@users.noreply.github.com>
2022-03-03 18:26:19 +0530

    Update selectionSort.java

--- SORTING/SELECTION SORT/selectionSort.java
+++ SORTING/SELECTION SORT/selectionSort.java
@@ -30,26 +30,6 @@ public class selectionSort {
         }
     }
     
-    void sort(int[] arr) {
-
-        int min;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-
-            min = i;
-
-            for (int j = i + 1; j < arr.length; j++) {
-
-                if (arr[j] < arr[min]) {
-
-                    min = j;
-                }
-            }
-        
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-    }
    
 }
     
